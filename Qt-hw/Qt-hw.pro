17f38c994878eb5c682c27c7bfa2eeb9874ce7cf
“sahinvitamu” <sahin@vitamu.com>
2023-05-23 11:11:52 +0300

    color game added.

--- Qt-hw/Qt-hw.pro
+++ Qt-hw/Qt-hw.pro
@@ -1,4 +1,12 @@
 QT += widgets
 
 SOURCES += \
+    colorbutton.cpp \
+    mygrid.cpp \
+    mytimer.cpp \
     simplequitter.cpp
+
+HEADERS += \
+    colorbutton.h \
+    mygrid.h \
+    mytimer.h
--- Qt-hw/simplequitter.cpp
+++ Qt-hw/simplequitter.cpp
@@ -1,30 +1,47 @@
 #include <QApplication>
-#include <QLabel>
-#include <QMainWindow>
-#include <QHBoxLayout>
-#include <QPushButton>
+#include <QVBoxLayout>
+#include <QSpacerItem>
+#include "colorbutton.h"
+#include "mytimer.h"
+#include "mygrid.h"
 
 
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc,argv);
-    QMainWindow *mw = new QMainWindow;
     QWidget *cw = new QWidget;
-    QHBoxLayout *hl = new QHBoxLayout(cw);
-    QLabel *ql = new QLabel;
-    QPushButton *pb = new QPushButton("Quit");
+    QVBoxLayout *vb = new QVBoxLayout(cw);
+    MyTimer mt;
+    MyGrid *gl = new MyGrid(mt.timer);
 
+    for(int row = 0; row<4;row++){
+        for(int col=0; col<3;col++){
+            int color_code = rand()%3;
+            QString color;
+            if(color_code==0){
+                color= "red";
+            }else if(color_code ==1){
+                color = "blue";
+            }else{
+                color = "green";
 
-    ql->setText("This is my first project.");
-    hl->addWidget(ql);
-    hl->addWidget(pb);
+            }
+            ColorButton *randButton = new ColorButton(color,"X");
+            QObject::connect(randButton, SIGNAL(clicked()),randButton,SLOT(change_color()));
+            QObject::connect(randButton, SIGNAL(clicked()),gl,SLOT(check_colors()));
+            gl->addWidget(randButton,row,col,1,1);
+        }
+    }
+    vb->addWidget(mt.label);
+    vb->addLayout(gl);
+    QSpacerItem *si = new QSpacerItem(0,10,QSizePolicy::Expanding,QSizePolicy::Expanding);
+    vb->addSpacerItem(si);
+    cw->setWindowTitle("Color Game");
+    cw->resize(640,480);
+    cw->show();
 
-
-    QObject::connect(pb,SIGNAL(clicked()),&app,SLOT(quit()));
-
-    mw->setCentralWidget(cw);
-    mw ->setWindowTitle("Hello World!");
-    mw->show();
     return app.exec();
+
+
 }
